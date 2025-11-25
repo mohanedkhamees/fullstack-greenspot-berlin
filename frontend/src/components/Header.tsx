60838907b4ffb72890455c6a69d6faf393278f77
mohaendkhamees <mohaned.khamees@Student.HTW-Berlin.de>
2025-11-26 00:59:25 +0100

    add logo and new info to readme

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,10 @@
 export const Header: React.FC = () => {
   return (
     
-   <header className="bg-gray-800 text-white px-6 py-4 shadow-md">
+   <header className="bg-blue-400 text-white px-6 py-4 shadow-md">
   <div className="flex flex-col items-center justify-center">
     <div className="flex items-center gap-2">
-      <img src="./asssets/logo.png" className="w-7 h-7" />
+      <img src="logo1.png" className="w-15 h-20" />
       <h1 className="text-2xl font-bold">
         Berlin Wandel by the WebSmithsProfis
       </h1>
